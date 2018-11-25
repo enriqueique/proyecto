a1f8cc181f294385fb6ce71854933f52d463f55c
enriqueique <43851195+enriqueique@users.noreply.github.com>
2018-11-25 23:16:57 +0100

    Add files via upload

---
+++ conexion/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    define('HOST', '198.91.81.7');
+    define('USER', 'sanepita_user');
+    define('PASS', '1234');
+    define('NAME', 'sanepita_prueba');
+    define('URL', 'http://sanepitafio.x10host.com/');
+
+?>
